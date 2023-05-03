fb64ad734696d1efb8d2389fc5146b59115c2bb4
Jeon-HS4 <jin0004@tukorea.ac.kr>
2023-05-03 16:54:30 +0900

    React chapter10 commit

--- src/index.js
+++ src/index.js
@@ -11,11 +11,12 @@ import NotificationList from './ch06/NotificationList';
 import Accomodate from './ch07/accommodate';
 import ConfirmButton from './ch08/ConfirmButton';
 import LandingPage from './ch09/LandingPage';
+import AttendanceBook from './ch10/AttendanceBook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LandingPage/>
+    <AttendanceBook/>
   </React.StrictMode>
 );
 
