dcb7acec5ec26cff76a0a38f2993be8dd012ddee
Jeon-HS4 <jin0004@tukorea.ac.kr>
2023-05-09 13:42:10 +0900

    React chapter13 commit

---
+++ src/ch13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props){
+    return (
+        <Card title = "HS-Jeon" backgoundColor="#4ea04e">
+            <p>안녕하세요 전형서입니다.</p>
+            <p>지금 리액트 공부중입니다.</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;
